1a3ee6ab7c09c48d5f7cb1a931c86eb0dba75709
jmnuf <jose.m.nufio.0@gmail.com>
2025-04-09 13:35:07 +0200

    [chore] Corregir titulo de php de completar pedido

--- update_pedido.php
+++ update_pedido.php
@@ -8,7 +8,7 @@ require_once(__DIR__ . '/connection.php');
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Crear Pedido</title>
+    <title>Completar Pedido</title>
   </head>
   <body>
     <?php
